8c2bbc21266c6b0ffcf934c5bc0d50fab68fd25e
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-08-25 03:12:38 +0800

    refactor: remove asterisk

--- src/app.cpp
+++ src/app.cpp
@@ -12,44 +12,44 @@ int main() {
   int isContinue;
 
   do {
-    // * Clean up the screen first.
+    // Clean up the screen first.
     clearScreen();
 
-    // * Application description.
+    // Application description.
     appDescription();
 
-    // * Ask input for total bus seats.
+    // Ask input for total bus seats.
     std::cout << endl << "Enter the total number of bus seats: ";
     std::cin >> totalSeats;
 
-    // * Ask input for total passengers.
+    // Ask input for total passengers.
     std::cout << "Enter the total number of passengers: ";
     std::cin >> totalPassengers;
 
     if (totalPassengers < totalSeats) {
-      // * If passengers less than total seats.
+      // If passengers less than total seats.
       emptySeats = totalSeats - totalPassengers;
       std::cout << "Total last trip empty seats: " << emptySeats << endl << endl;
       
-      // * Ask if want to continue.
+      // Ask if want to continue.
       std::cout << "Do you want to continue (1 - yes, 0 = no)?: ";
       std::cin >> isContinue;
     } else {
       emptySeats = totalPassengers % totalSeats;
       
       if (emptySeats == 0) {
-        // * If total seats is divisible by total passengers.
+        // If total seats is divisible by total passengers.
         std::cout << "Total last trip empty seats: " << emptySeats << endl << endl;
 
-        // * Ask if want to continue.
+        // Ask if want to continue.
         std::cout << "Do you want to continue (1 - yes, 0 = no)?: ";      
         std::cin >> isContinue;
       } else {
-        // * If total seats are not divisible by total passengers.
+        // If total seats are not divisible by total passengers.
         emptySeats = totalSeats - emptySeats;
         std::cout << "Total last trip empty seats: " << emptySeats << endl << endl;
         
-        // * Ask if want to continue
+        // Ask if want to continue
         std::cout << "Do you want to continue (1 - yes, 0 = no)?: ";      
         std::cin >> isContinue;
       }
